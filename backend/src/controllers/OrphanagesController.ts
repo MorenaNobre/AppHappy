dde1c105c664d846219cbada08c26ab29ccf2f82
MorenaNobre <morenagnobre@gmail.com>
2020-11-10 10:48:14 -0300

    restarting-upload-imgs

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response} from 'express'
+import { Request, Response} from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
 
