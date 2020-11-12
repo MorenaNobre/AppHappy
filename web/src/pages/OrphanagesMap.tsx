b7ffa6d10ef148ff7fe08ea74aba02ff76b97b41
MorenaNobre <morenagnobre@gmail.com>
2020-11-12 13:02:57 -0300

    sidebar-component-create-orphanage

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
-import 'leaflet/dist/leaflet.css'
+import "leaflet/dist/leaflet.css";
 
-import mapMarkerImg from '../images/marker-map.svg'
+import mapMarkerImg from "../images/marker-map.svg";
 
-import '../styles/pages/orphanages-map.css'
+import "../styles/pages/orphanages-map.css";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [48, 58],
+  iconAnchor: [24, 58],
+  popupAnchor: [170, 2],
+});
 
 // eslint-disable-next-line
 function OrphanagesMap() {
@@ -24,19 +32,35 @@ function OrphanagesMap() {
           <span>Sorocaba</span>
         </footer>
       </aside>
-      <MapContainer 
-        center={[-23.5173187,-47.4707938]}
+      <MapContainer
+        center={[-23.5173187, -47.4707938]}
         zoom={16}
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: "100%", height: "100%" }}
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        <Marker icon={mapIcon} position={[-23.5173187, -47.4707938]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das Meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
       </MapContainer>
-      <Link to="" className="create-orphanage">
-        <FiPlus size={32} color="#fff"/>
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
       </Link>
     </div>
-  )
+  );
 }
 
-export default OrphanagesMap
+export default OrphanagesMap;
