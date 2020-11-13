86fbfe2177c4738de00d039b1153338e4d7c4c85
MorenaNobre <morenagnobre@gmail.com>
2020-11-13 13:28:40 -0300

    create-orphanage-dynamic

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import React, { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+} from "react-leaflet";
+import { LeafletMouseEvent } from "leaflet";
 
 import { FiPlus } from "react-icons/fi";
 import Sidebar from "../components/Sidebar";
@@ -9,10 +14,19 @@ import mapIcon from "../utils/mapIcon";
 
 // eslint-disable-next-line
 export default function CreateOrphanage() {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    const { lat, lng } = event.latlng;
+
+    setPosition({
+      latitude: lat,
+      longitude: lng,
+    });
+  }
 
   return (
     <div id="page-create-orphanage">
-      
       <Sidebar />
 
       <main>
@@ -24,16 +38,15 @@ export default function CreateOrphanage() {
               center={[-27.2092052, -49.6401092]}
               style={{ width: "100%", height: 280 }}
               zoom={15}
-            >
+              onClick={handleMapClick}
+              >
               <TileLayer
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker
-                interactive={false}
-                icon={mapIcon}
-                position={[-27.2092052, -49.6401092]}
-              />
+              { position.latitude !== 0 
+              ? <Marker interactive={false} icon={mapIcon} position={[position.latitude, position.longitude]} /> 
+              : null }
             </MapContainer>
 
             <div className="input-block">
