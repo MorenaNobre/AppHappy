e6ef2f6aeca00ed7a143a9fea69ebd68ad7e160c
MorenaNobre <morenagnobre@gmail.com>
2020-11-16 08:56:22 -0300

    onClick-error-MapContainer

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import {
   MapContainer,
   Marker,
-  TileLayer,
+  TileLayer
 } from "react-leaflet";
 import { LeafletMouseEvent } from "leaflet";
 
@@ -35,10 +35,10 @@ export default function CreateOrphanage() {
             <legend>Dados</legend>
 
             <MapContainer
-              center={[-27.2092052, -49.6401092]}
+              center={[-23.4882334,-47.4791597]}
               style={{ width: "100%", height: 280 }}
               zoom={15}
-              onClick={handleMapClick}
+              // onClick={handleMapClick}
               >
               <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
